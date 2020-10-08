1f9a28e465eee0b331c4b89795836385bf7c7c9d
artemyudenko <artemyudenko2@gmail.com>
2020-10-08 17:28:59 +0200

    init commit

---
+++ src/main/kotlin/com/ayudenko/exchanger/ExchangerKtApplication.kt
@@ -0,0 +1,11 @@
+package com.ayudenko.exchanger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExchangerKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExchangerKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/ayudenko/exchanger/ExchangerKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ayudenko.exchanger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExchangerKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
